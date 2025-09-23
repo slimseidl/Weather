b842e9e7085a1d894492408a324f02219ee80039
John Seidl <jseidl2@wgu.edu>
2025-09-23 16:51:41 -0500

    Updated

--- Location.py
+++ Location.py
@@ -1,8 +1,19 @@
 from geopy.geocoders import Nominatim
 import requests
+from Weather import WeatherData
 
 
 geolocator = Nominatim(user_agent="weather_app")
-location = geolocator.geocode("Carroll, Iowa")
 
-print(location.latitude, location.longitude)
+
+city = input(f'Enter City: \n')
+state = input(f'Enter State: \n')
+location = geolocator.geocode(f'{city}, {state}')
+latitude = location.latitude
+longitude = location.longitude
+day = input()
+month = input()
+year = input()
+
+
+
